e4a021316ecaa201c4bd06c4a4fec0d2b40e8423
Avinash Kumar <avinashkumartirakachuri9@gmail.com>
2026-03-05 23:00:17 +0530

    My new Todo_List

---
+++ Todo_list/todolist.py
@@ -0,0 +1,11 @@
+def Todo():
+    dictionary={
+        1:"ADD TASK",
+        2:"VIEW TASK",
+        3:"REMOVE TASK",
+        4:"EXIT"
+    }
+    print(f'{1}:"ADD TASK"\n{2}:"VIEW TASK"\n{3}:"REMOVE TASK"\n{4}:"EXIT"')
+    a=int(input("Enter Which operation have to perform"))
+    
+Todo()
